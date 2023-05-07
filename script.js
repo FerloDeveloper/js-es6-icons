d17db74f220a66838e353cb1fbeaf8814b4487dd
FerloDeveloper <gianluca.laferla.dev@gmail.com>
2023-05-07 18:23:17 +0200

    Animal Icons

--- script.js
+++ script.js
@@ -15,122 +15,124 @@
 
 const icons = [
 	{
-		name: 'cat',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
+		name: "cat",
+		prefix: "fa-",
+		type: "animal",
+		family: "fas",
+		color: "orange",
 	},
 	{
-		name: 'crow',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
+		name: "crow",
+		prefix: "fa-",
+		type: "animal",
+		family: "fas",
+		color: "orange",
 	},
 	{
-		name: 'dog',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
+		name: "dog",
+		prefix: "fa-",
+		type: "animal",
+		family: "fas",
+		color: "orange",
 	},
 	{
-		name: 'dove',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
+		name: "dove",
+		prefix: "fa-",
+		type: "animal",
+		family: "fas",
+		color: "orange",
 	},
 	{
-		name: 'dragon',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
+		name: "dragon",
+		prefix: "fa-",
+		type: "animal",
+		family: "fas",
+		color: "orange",
 	},
 	{
-		name: 'horse',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
+		name: "horse",
+		prefix: "fa-",
+		type: "animal",
+		family: "fas",
+		color: "orange",
 	},
 	{
-		name: 'hippo',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
+		name: "hippo",
+		prefix: "fa-",
+		type: "animal",
+		family: "fas",
+		color: "orange",
 	},
 	{
-		name: 'fish',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas',
-		color: 'orange'
+		name: "fish",
+		prefix: "fa-",
+		type: "animal",
+		family: "fas",
+		color: "orange",
 	},
 	{
-		name: 'carrot',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas',
-		color: 'green'
+		name: "carrot",
+		prefix: "fa-",
+		type: "vegetable",
+		family: "fas",
+		color: "green",
 	},
 	{
-		name: 'apple-alt',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas',
-		color: 'green'
+		name: "apple-alt",
+		prefix: "fa-",
+		type: "vegetable",
+		family: "fas",
+		color: "green",
 	},
 	{
-		name: 'lemon',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas',
-		color: 'green'
+		name: "lemon",
+		prefix: "fa-",
+		type: "vegetable",
+		family: "fas",
+		color: "green",
 	},
 	{
-		name: 'pepper-hot',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas',
-		color: 'green'
+		name: "pepper-hot",
+		prefix: "fa-",
+		type: "vegetable",
+		family: "fas",
+		color: "green",
 	},
 	{
-		name: 'user-astronaut',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas',
-		color: 'blue'
+		name: "user-astronaut",
+		prefix: "fa-",
+		type: "user",
+		family: "fas",
+		color: "blue",
 	},
 	{
-		name: 'user-graduate',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas',
-		color: 'blue'
+		name: "user-graduate",
+		prefix: "fa-",
+		type: "user",
+		family: "fas",
+		color: "blue",
 	},
 	{
-		name: 'user-ninja',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas',
-		color: 'blue'
+		name: "user-ninja",
+		prefix: "fa-",
+		type: "user",
+		family: "fas",
+		color: "blue",
 	},
 	{
-		name: 'user-secret',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas',
-		color: 'blue'
-	}
+		name: "user-secret",
+		prefix: "fa-",
+		type: "user",
+		family: "fas",
+		color: "blue",
+	},
 ];
 
 function printIcon(container, object) {
-    const icon = document.querySelector("#tpl-icon").content.cloneNode(true);
-	icon.querySelector(".box i").classList.add(object.family, object.prefix + object.name);
+	const icon = document.querySelector("#tpl-icon").content.cloneNode(true);
+	icon
+		.querySelector(".box i")
+		.classList.add(object.family, object.prefix + object.name);
 	icon.querySelector(".box i").style.color = object.color;
 	icon.querySelector(".name").innerHTML = object.name;
 	container.append(icon);
@@ -142,11 +144,13 @@ icons.forEach((elm) => printIcon(iconContainer, elm));
 
 const selectElement = document.querySelector("#typo");
 
-selectElement.addEventListener('change', 
+selectElement.addEventListener(
+	"change",
 
 	function () {
-		
-		let iconsFiltered = icons.filter(elm => elm.type === selectElement.value || selectElement.value==="all");
+		let iconsFiltered = icons.filter(
+			(elm) => elm.type === selectElement.value || selectElement.value === "all"
+		);
 		iconContainer.innerHTML = " ";
 		iconsFiltered.forEach((elm) => printIcon(iconContainer, elm));
 	}
